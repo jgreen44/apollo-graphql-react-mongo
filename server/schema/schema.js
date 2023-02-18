8d2ed572c82b8987d326dd7404b044e79f544459
Jason Green <jgreen44@asu.edu>
2023-02-18 16:31:27 -0600

    added schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,6 +8,7 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLList,
+  GraphQLNonNull,
 } = require('graphql');
 
 // Project Type
@@ -39,9 +40,11 @@ const ClientType = new GraphQLObjectType({
   }),
 });
 
+// Root Query
 const rootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    // Get all clients
     clients: {
       type: new GraphQLList(ClientType),
       resolve() {
@@ -49,6 +52,7 @@ const rootQuery = new GraphQLObjectType({
         return Client.find();
       },
     },
+    // Get single client
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
@@ -57,6 +61,7 @@ const rootQuery = new GraphQLObjectType({
         return Client.findById(args.id);
       },
     },
+    // Get all projects
     projects: {
       type: new GraphQLList(ProjectType),
       resolve() {
@@ -64,6 +69,7 @@ const rootQuery = new GraphQLObjectType({
         return Project.find();
       },
     },
+    // Get single project
     project: {
       type: ProjectType,
       args: { id: { type: GraphQLID } },
@@ -75,6 +81,41 @@ const rootQuery = new GraphQLObjectType({
   },
 });
 
+// Mutations
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    // Add Client
+    addClient: {
+      type: ClientType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        email: { type: GraphQLNonNull(GraphQLString) },
+        phone: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        const client = new Client({
+          name: args.name,
+          email: args.email,
+          phone: args.phone,
+        });
+        return client.save();
+      },
+    },
+    // Delete Client
+    deleteClient: {
+      type: ClientType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        return Client.findByIdAndRemove(args.id);
+      },
+    },
+  },
+});
+
 module.exports = new GraphQLSchema({
   query: rootQuery,
+  mutation,
 });
